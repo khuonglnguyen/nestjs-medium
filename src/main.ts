b03950c5cac7fefafb41e4db5f174da69adbbfa9
victornguyen0412 <victor.nguyen@beyondedge.co>
2023-04-08 11:02:35 +0700

    Configuring absolute path

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
+if (!process.env.IS_TS_NODE) {
+  require('module-alias/register');
+}
+
+console.log('process.env.IS_TS_NODE',process.env.IS_TS_NODE);
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
