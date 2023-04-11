21ec779515a0f4e09eab39b7e6f40aa287d5dc8a
victornguyen0412 <victor.nguyen@beyondedge.co>
2023-04-11 20:04:30 +0700

    Add tag entity

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,12 +1,14 @@
 import { ConnectionOptions } from 'typeorm';
 
 const config: ConnectionOptions = {
-    type:'postgres',
-    host:'localhost',
-    port: 5432,
-    username:'postgres',
-    password: 'aA123!@#',
-    database:'medium'
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'aA123!@#',
+  database: 'medium',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: true,
 };
 
 export default config;
